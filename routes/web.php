543bdf7161bf12d7aa58b57090f7d7d2b275b225
daaeev <LfdblGbhju@gmail.com>
2022-08-18 20:34:42 +0300

    Реализована аутентификация laravel/ui

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,8 @@
 <?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
